d7f36612998a389cf8ef21e24f2de8f459b29be7
po160501 <notdots64589@gmail.com>
2026-08-04 00:01:25 -0500

    cambio para mostrar el login

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return auth()->check()
+        ? redirect()->route('dashboard')
+        : redirect()->route('login');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
